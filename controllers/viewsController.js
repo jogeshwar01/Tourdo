04293d48b07f51c87d18a36e091df6cae6baa0a5
Jogeshwar Singh <singhjogeshwar007@gmail.com>
2021-12-21 15:15:21 +0530

    build the tour overview page

--- controllers/viewsController.js
+++ controllers/viewsController.js
@@ -1,9 +1,19 @@
+const Tour = require('../models/tourModel');
+const catchAsync = require('../utils/catchAsync');
+const AppError = require('../utils/appError');
+
 //no need to specify .pug here and it will look in the foler specified as the views path at the top
-exports.getOverview = (req, res) => {
+exports.getOverview = catchAsync(async (req, res, next) => {
+    // 1) Get tour data from collection
+    const tours = await Tour.find();
+
+    // 2) Build template    --in the pug file
+    // 3) Render that template using tour data from 1)
     res.status(200).render('overview', {
-        title: 'All Tours'
+        title: 'All Tours',
+        tours
     });
-};
+});
 
 exports.getTour = (req, res) => {
     res.status(200).render('tour', {
